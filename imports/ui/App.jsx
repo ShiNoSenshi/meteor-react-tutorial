68e5bdffd7357b292945e66b150fbf324573c697
ShiNoSenshi <darksenshi@gmail.com>
2019-07-05 15:21:07 +0200

    client can hide finished tasks.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,14 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,
+        };
+    }
+
     handleSubmit(event) {
         event.preventDefault();
 
@@ -23,8 +31,18 @@ class App extends Component {
         textInput.value = '';
     }
 
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
+
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked)
+        }
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task} />
         ))
     }
@@ -33,6 +51,11 @@ class App extends Component {
         return (
             <div className="container">
                 <header><h1>Todo List</h1></header>
+
+                <label className="hide-completed">
+                    <input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)} />
+                    Hide Completed Tasks
+                </label>
 
                 <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
                     <input type="text" ref="textInput" placeholder="Type to add new tasks" />
