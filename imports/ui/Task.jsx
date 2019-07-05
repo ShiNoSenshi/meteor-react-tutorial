dd20d11f824e3b08c083897435644449722dc533
ShiNoSenshi <darksenshi@gmail.com>
2019-07-05 17:33:32 +0200

    implemented tasks.setPrivate

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -11,6 +11,11 @@ export default class Task extends Component {
         Meteor.call('tasks.remove', this.props.task._id);
     }
 
+    togglePrivate() {
+        // Set the checked property to the opposite of its current value
+        Meteor.call('tasks.setPrivate', this.props.task._id, !this.props.task.private);
+    }
+
     render() {
         // Give tasks a different className when they are checked off,
         // so that we can style them nicely in CSS
@@ -28,6 +33,12 @@ export default class Task extends Component {
                     checked={!!this.props.task.checked}
                     onClick={this.toggleChecked.bind(this)}
                 />
+
+                { this.props.showPrivateButton ? (
+                    <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+                        { this.props.task.private ? 'Private' : 'Public' }
+                    </button>
+                ) : ''}
 
                 <span className="text"><strong>{this.props.task.username}</strong>: {this.props.task.text}</span>
             </li>
