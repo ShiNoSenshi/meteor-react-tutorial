d2d4f70ab735267120434ab3580b37da05a04b1b
ShiNoSenshi <darksenshi@gmail.com>
2019-07-05 15:10:38 +0200

    tasks can be checked.

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,10 +1,32 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+
+import { Tasks } from '../api/tasks.js';
 
 //Taks component - represents a single item
 export default class Task extends Component {
+    toggleChecked() {
+        // Set the checked property to the opposite of its current value
+        Tasks.update(this.props.task._id, {
+            $set: { checked: !this.props.task.checked },
+        });
+    }
+
     render() {
+        // Give tasks a different className when they are checked off,
+        // so that we can style them nicely in CSS
+        const taskClassName = this.props.task.checked ? 'checked' : '';
+
         return (
-            <li>{this.props.task.text}</li>
-        )
+            <li className={taskClassName}>
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={!!this.props.task.checked}
+                    onClick={this.toggleChecked.bind(this)}
+                />
+
+                <span className="text">{this.props.task.text}</span>
+            </li>
+        );
     }
 }
